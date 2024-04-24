794f83f7be23216041f5e2d803c679fa85bc1e21
Bayron Fajardo <bayronfajardor@gmail.com>
2024-04-24 16:45:52 -0500

    [Bayron Fajardo] Agregue Aplicacion Universidad

---
+++ Universidad/Academico/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    	path('', views.home, name='home'),
+        path('registrarCurso/', views.registrarCurso),
+        path('edicionCurso/<codigo>', views.edicionCurso),
+        path('editarCurso/', views.editarCurso),
+        path('eliminacionCurso/<codigo>', views.eliminarCurso)
+]
